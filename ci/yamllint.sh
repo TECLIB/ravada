731aed72ac5e39e3881058375f72309c1480ff38
Gianfranco Manganiello <gmanganiello@teclib.com>
2018-09-06 17:32:08 -0400

    docs(winstrap): create an exclusive sass configuration file
    Signed-off-by: Gianfranco Manganiello <gmanganiello@teclib.com>

--- ci/yamllint.sh
+++ ci/yamllint.sh
@@ -1,5 +1,4 @@
 ./node_modules/.bin/yamllint _config.yml
 ./node_modules/.bin/yamllint .sass-lint.yml
-./node_modules/.bin/yamllint .circleci/config.yml
 ./node_modules/.bin/yamllint _data/config.yml
 ./node_modules/.bin/yamllint _data/whitelist_GP.yml
